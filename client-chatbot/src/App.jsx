d9a8052a9ee63aea6cc494c6e4d8bf06a1d1cc31
Sandeep Kumar Mahato <90895228+SandeepKumar2024@users.noreply.github.com>
2023-12-13 16:40:37 +0000

    updated code

--- client-chatbot/src/App.jsx
+++ client-chatbot/src/App.jsx
@@ -1,37 +1,84 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './app.scss'
+import back from "./img/new.jpg"
 
 function App() {
+
+  const [displayText, setDisplayText] = useState('');
+  const originalText = "PRAKRITI CHATBOT";
+
+  const [topPosition, setTopPosition] = useState(0);
+  const [movingUp, setMovingUp] = useState(true);
+
+  useEffect(() => {
+    const animationInterval = setInterval(() => {
+      const newPosition = movingUp ? topPosition - 1 : topPosition + 1;
+
+      setTopPosition(newPosition);
+
+      // Change direction when reaching a certain position (e.g., -100px)
+      if (newPosition <=100) {
+        setMovingUp(false);
+      } else if (newPosition >= 300) {
+        setMovingUp(true);
+      }
+    }, 50); // Adjust the interval as needed for smoother animation
+
+    return () => clearInterval(animationInterval);
+  }, [topPosition, movingUp]);
+
+
+  useEffect(() => {
+    let index = 0;
+
+    const intervalId = setInterval(() => {
+      setDisplayText(originalText.substring(0, index));
+      index++;
+
+      if (index > originalText.length) {
+        clearInterval(intervalId);
+      }
+    }, 100); 
+    return () => clearInterval(intervalId);
+  }, []);
   return (
    <>
    <div className="container">
+    <div className="navbar">
+      <div className="leftNav">
+        <span>Prakriti <i style={{color:'orange'}}>Chatbot</i></span>
+      </div>
+      <div className="rightNav">
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>Doctors</li>
+          <li>About</li>
+        </ul>
+      </div>
+    </div>
+    <div className="wrapper">
+
+    
    <div className="left">
-    <h1>WELCOME TO PRAKRITI CHATBOT </h1>
+    <h1>WELCOME TO <i>{displayText} </i></h1>
+    <p>
+          Prakrti Chatbot is your personal assistant to help you understand and balance your doshas.
+          Start a conversation to explore more about Ayurveda and how it can benefit your well-being.
+        </p>
+        <button>Start Chatting</button>
+
   </div>
   <div className="right">
-    <div className="tRight">
-      <div className="logo">
-       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
-       </svg>
-
-        <p>Hello I am sara </p>
-      </div>
-    </div>
-    <div className="mRight">midddle</div>
-    <div className="bRight">
-      <div className="form">
-        <form action="">
-          <input type="text" placeholder='Type Here' />
-          <button>Send</button>
-        </form>
-      </div>
-    </div>
+  <img src={back} alt="" style={{top:`${topPosition}px`}}/>
+    
+    
+    
 
  
   </div>
 
-
+  </div>
    </div>
   </>
   )
